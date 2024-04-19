0eeba6c759cbfd03c73d8e4cc89bc1d18cfa91df
Melbin Koshy <melbinkoshy2002@gmail.com>
2024-04-19 23:22:55 +0530

    new commit

--- ecommerce/ecommerce/settings/base.py
+++ ecommerce/ecommerce/settings/base.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+
 from dotenv import load_dotenv
+
 load_dotenv()
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
